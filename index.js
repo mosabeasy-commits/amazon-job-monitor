3e3992a7845d4c7127d83e4e02f4c520f66fbf57
mosabeasy-commits <mosabeasy@gmail.com>
2026-05-27 04:45:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-console.log("amazon-job-monitor started");
+const http = require("http");
 
-setInterval(() => {
-  console.log("running...");
-}, 60000);
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("amazon-job-monitor running");
+}).listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
